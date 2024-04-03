16c59c25d11d3e3acfdee751f9c155b8d91e68c6
irving2delgado <irving2delgado@gmail.com>
2024-04-02 19:23:36 -0600

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import tailwindcss from 'tailwindcss';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    postcss: {
+        plugins: [
+          tailwindcss,
+          // Add any other PostCSS plugins if needed
+        ],
+      },
+});
