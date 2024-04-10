eb3d4ebf441aa0a336cca9b5b48177457faa3c53
irving2delgado <irving2delgado@gmail.com>
2024-04-10 14:37:41 -0600

    ejercicio semanal 7

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from 'tailwindcss';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    postcss: {
-        plugins: [
-          tailwindcss,
-          // Add any other PostCSS plugins if needed
-        ],
-      },
+  postcss: {
+    plugins: [tailwindcss
+    // Add any other PostCSS plugins if needed
+    ]
+  },
+  integrations: [tailwind()]
 });
